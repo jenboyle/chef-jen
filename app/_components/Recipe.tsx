32426c3764957e0175b4f37ed2697545431ae76a
jenboyle <groovyjen60@hotmail.com>
2025-04-07 17:49:17 +0100

    Add toggle for vegetarian option

---
+++ app/_components/Recipe.tsx
@@ -0,0 +1,85 @@
+"use client";
+import Image from "next/image";
+import RecipeCardTools from "./RecipeCardTools";
+import RecipeCardTitle from "./RecipeCardTitle";
+import RecipeCardIngredients from "./RecipeCardIngredients";
+import { recipeType, userType } from "./RecipeCard";
+
+interface RecipeProps {
+  recipe: recipeType;
+  index: number;
+  isAuthenticated: boolean;
+}
+
+function Recipe({ recipe, index, isAuthenticated }: RecipeProps) {
+  return (
+    <div
+      className="bg-yellow-50 p-2 m-4 border border-stone-300 relative"
+      key={`${index} ${recipe.name}`}
+    >
+      {recipe.bg_img ? (
+        <Image
+          src={recipe.bg_img}
+          className="opacity-10 object-cover"
+          fill
+          alt={recipe.name}
+          quality={70}
+        ></Image>
+      ) : null}
+      <RecipeCardTitle
+        recipeName={recipe.name}
+        vegetarian={recipe.vegetarian}
+      />
+      <RecipeCardIngredients ingredients={recipe.ingredients} />
+      <h4 className="text-lg mb-8">
+        <span className="font-semibold">Method:</span> {recipe.method}
+      </h4>
+      <div className="w-full relative h-auto">
+        {recipe.img ? (
+          <Image
+            src={recipe.img}
+            className="w-4/5 max-w-2xl h-auto !relative"
+            alt={recipe.name}
+            fill
+            quality={90}
+          ></Image>
+        ) : null}
+      </div>
+      {recipe.points ? (
+        <h5 className="text-base my-2">
+          <span className="font-semibold">Ww old 🏫 points: </span>
+          <span>
+            {recipe.points}{" "}
+            {recipe.points_per ? <span> per {recipe.points_per}</span> : null}
+          </span>
+        </h5>
+      ) : null}
+      {recipe.calories ? (
+        <h6 className="text-base mb-8">
+          <span className="font-semibold">Calories: </span>
+          <span>
+            {recipe.calories}
+            {recipe.calories_per ? (
+              <span>kcal per {recipe.calories_per}</span>
+            ) : null}
+          </span>
+        </h6>
+      ) : null}
+      {recipe.serves ? (
+        <p className="text-base mb-2">
+          <span className="font-semibold">Serves: </span>
+          {recipe.serves}
+        </p>
+      ) : null}
+      {(recipe.users as unknown as userType)!.username ? (
+        <p className="text-base mb-2">
+          <span className="font-semibold">Created by:</span>{" "}
+          {(recipe.users as unknown as userType)!.username}
+        </p>
+      ) : null}
+      <RecipeCardTools isDisabled={!isAuthenticated || recipe.readonly} />
+    </div>
+  );
+}
+
+export default Recipe;
--- app/_components/RecipeCard.tsx
+++ app/_components/RecipeCard.tsx
@@ -1,14 +1,13 @@
 "use client";
-import Image from "next/image";
-import RecipeCardTools from "./RecipeCardTools";
 import { ChangeEvent, useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import RecipeCardTitle from "./RecipeCardTitle";
-import RecipeCardIngredients from "./RecipeCardIngredients";
 import PostAddIcon from "@mui/icons-material/PostAdd";
 import { IconButton, Tooltip } from "@mui/material";
 import { useUser } from "./authentication/useUser";
 import AddRecipe from "./AddRecipe";
+import Recipe from "./Recipe";
+import ToggleOffIcon from "@mui/icons-material/ToggleOff";
+import ToggleOnIcon from "@mui/icons-material/ToggleOn";
 
 export type userType = {
   username: string;
@@ -42,6 +41,7 @@ function RecipeCard({ recipes }: RecipeProps) {
   const [recipesFiltered, setRecipesFiltered] = useState(recipes);
   const { isAuthenticated } = useUser();
   const [openAddRecipe, setOpenAddRecipe] = useState(false);
+  const [showVeg, setShowVeg] = useState(false);
 
   useEffect(() => {
     if (recipes[0].recipe_type === "main") {
@@ -57,6 +57,10 @@ function RecipeCard({ recipes }: RecipeProps) {
 
   function handleCloseAddRecipe() {
     setOpenAddRecipe(false);
+  }
+
+  function handleToggleVeggie() {
+    setShowVeg(!showVeg);
   }
 
   function handleSearch(e: ChangeEvent<HTMLInputElement>) {
@@ -82,6 +86,17 @@ function RecipeCard({ recipes }: RecipeProps) {
   return (
     <>
       <div className="text-right mr-4">
+        <Tooltip title="Vegetarian">
+          <span>
+            <IconButton onClick={handleToggleVeggie}>
+              {showVeg ? (
+                <ToggleOnIcon sx={{ color: "green" }} />
+              ) : (
+                <ToggleOffIcon />
+              )}
+            </IconButton>
+          </span>
+        </Tooltip>
         <Tooltip title="Add Recipe">
           <span>
             <IconButton
@@ -103,76 +118,25 @@ function RecipeCard({ recipes }: RecipeProps) {
           className="p-2 m-2 font-semibold rounded-xl focus:outline-none hover:bg-blue-50 transition-colors duration-200 focus:ring focus:ring-yellow-200 focus:bg-blue-50 focus:ring-offset-2"
         ></input>
       </div>
-      {recipesFiltered.map((recipe, index) => (
-        <div
-          className="bg-yellow-50 p-2 m-4 border border-stone-300 relative"
-          key={`${index} ${recipe.name}`}
-        >
-          {recipe.bg_img ? (
-            <Image
-              src={recipe.bg_img}
-              className="opacity-10 object-cover"
-              fill
-              alt={recipe.name}
-              quality={70}
-            ></Image>
-          ) : null}
-          <RecipeCardTitle
-            recipeName={recipe.name}
-            vegetarian={recipe.vegetarian}
+      {recipesFiltered.map((recipe, index) =>
+        showVeg ? (
+          recipe.vegetarian ? (
+            <Recipe
+              key={`${index} ${recipe.name}`}
+              recipe={recipe}
+              index={index}
+              isAuthenticated={isAuthenticated}
+            />
+          ) : null
+        ) : (
+          <Recipe
+            key={`${index} ${recipe.name}`}
+            recipe={recipe}
+            index={index}
+            isAuthenticated={isAuthenticated}
           />
-          <RecipeCardIngredients ingredients={recipe.ingredients} />
-          <h4 className="text-lg mb-8">
-            <span className="font-semibold">Method:</span> {recipe.method}
-          </h4>
-          <div className="w-full relative h-auto">
-            {recipe.img ? (
-              <Image
-                src={recipe.img}
-                className="w-4/5 max-w-2xl h-auto !relative"
-                alt={recipe.name}
-                fill
-                quality={90}
-              ></Image>
-            ) : null}
-          </div>
-          {recipe.points ? (
-            <h5 className="text-base my-2">
-              <span className="font-semibold">Ww old 🏫 points: </span>
-              <span>
-                {recipe.points}{" "}
-                {recipe.points_per ? (
-                  <span> per {recipe.points_per}</span>
-                ) : null}
-              </span>
-            </h5>
-          ) : null}
-          {recipe.calories ? (
-            <h6 className="text-base mb-8">
-              <span className="font-semibold">Calories: </span>
-              <span>
-                {recipe.calories}
-                {recipe.calories_per ? (
-                  <span>kcal per {recipe.calories_per}</span>
-                ) : null}
-              </span>
-            </h6>
-          ) : null}
-          {recipe.serves ? (
-            <p className="text-base mb-2">
-              <span className="font-semibold">Serves: </span>
-              {recipe.serves}
-            </p>
-          ) : null}
-          {(recipe.users as unknown as userType)!.username ? (
-            <p className="text-base mb-2">
-              <span className="font-semibold">Created by:</span>{" "}
-              {(recipe.users as unknown as userType)!.username}
-            </p>
-          ) : null}
-          <RecipeCardTools isDisabled={!isAuthenticated || recipe.readonly} />
-        </div>
-      ))}
+        )
+      )}
     </>
   );
 }
